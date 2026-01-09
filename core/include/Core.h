f2a3ac944d907d14eac63c59aac2950ceaf2d289
fabio-terranova <fabio.terranova@outlook.it>
2026-01-09 01:21:25 +0100

    feat: add version output

--- core/include/Core.h
+++ core/include/Core.h
@@ -4,6 +4,9 @@
 #include <string>
 
 namespace Noddy {
+namespace Core {
+inline std::string_view version() { return "0.1.0"; };
+} // namespace Core
 } // namespace Noddy
 
 #endif // INCLUDE_CORE_CORE_H_
